12e451acc67a31e757cf21ac6fa6ea748de98308
Frank Jacobo <frankejacobos@gmail.com>
2022-09-05 12:37:25 -0500

    make it better

---
+++ src/modules/auth/store/state.js
@@ -0,0 +1,6 @@
+export default () => ({
+  status: "authenticating" /* authenticating, authenticated, unauthenticated */,
+  user: null,
+  idToken: null,
+  refreshToken: null,
+});
---
+++ src/modules/auth/views/RegisterView.vue
@@ -0,0 +1,68 @@
+<template>
+  <span class="login100-form-title p-b-41"> Ingresar </span>
+  <form
+    class="login100-form validate-form p-b-33 p-t-5"
+    @submit.prevent="onSubmit"
+  >
+    <div class="wrap-input100 validate-input" data-validate="Enter name">
+      <input
+        required
+        type="text"
+        class="input100"
+        placeholder="name"
+        v-model="userForm.name"
+      />
+      <span class="focus-input100" data-placeholder="&#xe82a;"></span>
+    </div>
+
+    <div class="wrap-input100 validate-input" data-validate="Enter email">
+      <input
+        required
+        type="email"
+        class="input100"
+        placeholder="email"
+        v-model="userForm.email"
+      />
+      <span class="focus-input100" data-placeholder="&#xe818;"></span>
+    </div>
+
+    <div class="wrap-input100 validate-input" data-validate="Enter password">
+      <input
+        required
+        type="password"
+        class="input100"
+        placeholder="password"
+        v-model="userForm.password"
+      />
+      <span class="focus-input100" data-placeholder="&#xe80f;"></span>
+    </div>
+
+    <div class="container-login100-form-btn m-t-32">
+      <button class="login100-form-btn" type="submit">Sign Up</button>
+    </div>
+
+    <div class="container-login100-form-btn m-t-32">
+      <router-link :to="{ name: 'login' }">¿Ya tienes cuenta?</router-link>
+    </div>
+  </form>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  setup() {
+    const userForm = ref({
+      name: "",
+      email: "",
+      password: "",
+    });
+    return {
+      userForm,
+      onSubmit: async () => console.log(userForm.value),
+    };
+  },
+};
+</script>
+
+<style></style>
